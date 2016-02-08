34322d64c323e7c295b8ebfbf0aba92e2b8c78c3
alberto <a>
2016-02-07 22:53:03 -0600

    Definicion de protocolo

--- src/aplicaciones/Cliente.java
+++ src/aplicaciones/Cliente.java
@@ -7,7 +7,9 @@ package aplicaciones;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  *
@@ -15,12 +17,30 @@ import java.net.Socket;
  */
 public class Cliente {
     
+    
     public static void main(String args[]){
+        String nombre;
+        String dificultad;
         try(Socket s = new Socket("localhost", 5555)){
+            nombre = null;
+            dificultad = null;
             BufferedReader bf;
+            PrintWriter pw;
+            
             bf = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            pw = new PrintWriter(s.getOutputStream(), true);
+            Scanner in = new Scanner(System.in);
             
             System.out.println(bf.readLine());
+            
+            System.out.println("¿Tu qué, quién te crees?");
+            nombre = in.nextLine();
+            
+            System.out.println("¿Qué tan picud@ te sientes?");
+            dificultad = in.nextLine();
+            
+            pw.println(nombre + "," + dificultad);
+            
         }
         catch(Exception e){
             
