a806273b689fc1e89a2d09f604af183528874d33
alberto <a>
2016-02-07 22:53:09 -0600

    Definicion de protocolo

--- src/aplicaciones/Servidor.java
+++ src/aplicaciones/Servidor.java
@@ -32,6 +32,8 @@ public class Servidor {
                 
                 pw.println(BIENVENIDA);
                // bienvenida(pw);
+               
+               System.out.println(bf.readLine());
                 
            }
            catch(Exception e){
